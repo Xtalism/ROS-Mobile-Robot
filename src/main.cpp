679304dae68a1862596190281689843f380e4dcb
xtalism <manuel_lego1234@hotmail.com>
2024-10-19 16:41:32 -0600

    optimized code

--- src/main.cpp
+++ src/main.cpp
@@ -2,21 +2,25 @@
 #include <ros.h>
 #include <sensor_msgs/Joy.h>
 
-const int firstMotorOne = 17;  // forward
-const int firstMotorTwo = 16;  // backward
+const int firstMotorOne = 16;  // forward
+const int firstMotorTwo = 17;  // backward
 const int speedMotorOne = 5;   // enable
 
-const int secondMotorOne = 4;  // forward
-const int secondMotorTwo = 0;  // backward
-const int speedMotorTwo = 2;   // enable
+const int secondMotorOne = 0;  
+const int secondMotorTwo = 4;  
+const int speedMotorTwo = 2;   
 
-const int fourthMotorOne = 27;  // forward
-const int fourthMotorTwo = 26;  // backward
-const int speedMotorFour = 25; // enable
+const int thirdMotorOne = 14;  
+const int thirdMotorTwo = 12;  
+const int speedMotorThree = 13;
 
-const int thirdMotorOne = 12;  // forward
-const int thirdMotorTwo = 14;  // backward
-const int speedMotorThree = 13;  // enable
+const int fourthMotorOne = 26;  
+const int fourthMotorTwo = 27;  
+const int speedMotorFour = 25; 
+
+int motorOnePins[] = {firstMotorOne, secondMotorOne, thirdMotorOne, fourthMotorOne};
+int motorTwoPins[] = {firstMotorTwo, secondMotorTwo, thirdMotorTwo, fourthMotorTwo};
+int motorSpeedPins[] = {speedMotorOne, speedMotorTwo, speedMotorThree, speedMotorFour};
 
 const char* ssid = "MotherBase"; 
 const char* password = "@6830135@"; 
@@ -27,111 +31,60 @@ const uint16_t serverPort = 11411;
 WiFiClient client;
 ros::NodeHandle nh; 
 
-void stopFirstMotor() {
-    digitalWrite(firstMotorOne, LOW);
-    digitalWrite(firstMotorTwo, LOW);
-    analogWrite(speedMotorOne, 0);
-}
-
-void stopSecondMotor() {
-    digitalWrite(secondMotorOne, LOW);
-    digitalWrite(secondMotorTwo, LOW);
-    analogWrite(speedMotorTwo, 0);
-}
-
-void stopThirdMotor() {
-    digitalWrite(thirdMotorOne, LOW);
-    digitalWrite(thirdMotorTwo, LOW);
-    analogWrite(speedMotorThree, 0);
-}
-
-void stopFourthMotor() {
-    digitalWrite(fourthMotorOne, LOW);
-    digitalWrite(fourthMotorTwo, LOW);
-    analogWrite(speedMotorFour, 0);
-}
-
-void controlMotorForwardBackward(int motorOnePin, int motorTwoPin, int motorSpeedPin, float stickY, float stickX, float RT, float LT) {
-    const int stickForward = 1;
-    const int stickBackward = -1;
-
-    const int stickLeft = 1;
-    const int stickRight = -1;
-
-    int motorStateFB = (stickY > 0.1) ? stickForward : (stickY < -0.1) ? stickBackward : 0;
-    int motorStateLR = (stickX > 0.1) ? stickLeft : (stickX < -0.1) ? stickRight : 0;
-
-    switch (motorStateFB) {
-        case stickForward:
-            digitalWrite(motorOnePin, HIGH);
-            digitalWrite(motorTwoPin, LOW);
-            analogWrite(motorSpeedPin, (RT * -128) + (LT * 128)); // break and speed
-            break;
-        case stickBackward:
-            digitalWrite(motorOnePin, LOW);
-            digitalWrite(motorTwoPin, HIGH);
-            analogWrite(motorSpeedPin, (RT * -128) + (LT * 128)); // break and speed
-            break;
-        default:
-            digitalWrite(motorOnePin, LOW);
-            digitalWrite(motorTwoPin, LOW);
-            analogWrite(motorSpeedPin, 0);
-            break;
+void stopMotors(int motorOnePins[], int motorTwoPins[], int motorSpeedPins[]) {
+    for (int i = 0; i < 4; i++) {
+        digitalWrite(motorOnePins[i], LOW);
+        digitalWrite(motorTwoPins[i], LOW);
+        digitalWrite(motorSpeedPins[i], 0);
     }
+}
 
-    switch (motorStateLR) {
-        case stickLeft:
-            digitalWrite(motorOnePin, LOW);
-            digitalWrite(motorTwoPin , HIGH);
-            analogWrite(motorSpeedPin, (RT * -128) + (LT * 128)); // break and speed
-            break;
-        case stickRight:
-            digitalWrite(motorOnePin, HIGH);
-            digitalWrite(motorTwoPin, LOW);
-            analogWrite(motorSpeedPin, (RT * -128) + (LT * 128)); // break and speed
-            break;
-        default:
-            digitalWrite(motorOnePin, LOW);
-            digitalWrite(motorTwoPin, LOW);
-            analogWrite(motorSpeedPin, 0);
-            break;
+void controlMotor(int motorOnePins[], int motorTwoPins[], int motorSpeedPins[], float stickY, float RT, float LT) {
+    const int stickValuePositive = 1;
+    const int stickValueNegative = -1;
+
+    int motorState = (stickY > 0.1) ? stickValuePositive : (stickY < -0.1) ? stickValueNegative : 0;
+
+    for (int i = 0; i < 4; i++) {
+        switch (motorState) {
+            case stickValuePositive:
+                digitalWrite(motorOnePins[i], HIGH);
+                digitalWrite(motorTwoPins[i], LOW);
+                analogWrite(motorSpeedPins[i], (RT * -255) + (LT * 255)); // break and speed
+                break;
+            case stickValueNegative:
+                digitalWrite(motorOnePins[i], LOW);
+                digitalWrite(motorTwoPins[i], HIGH);
+                analogWrite(motorSpeedPins[i], (RT * -255) + (LT * 255)); // break and speed
+                break;
+            default:
+                stopMotors(motorOnePins, motorTwoPins, motorSpeedPins);
+                break;
+        }
     }
 }
 
 void joyCallback(const sensor_msgs::Joy &joy_msg) {
-    float leftStickY = joy_msg.axes[4];
-    float rightStickY = joy_msg.axes[3];
-    
-    float rightStickX = joy_msg.axes[0];
-    float leftStickX = joy_msg.axes[1];
+    float leftStickY = joy_msg.axes[1];
+    float rightStickY = joy_msg.axes[4];
 
-    float RT = joy_msg.axes[5];
+    float leftStickX = joy_msg.axes[0]; 
+    float rightStickX = joy_msg.axes[3];
+
     float LT =  joy_msg.axes[2];
+    float RT = joy_msg.axes[5];
 
-    controlMotorForwardBackward(firstMotorOne, firstMotorTwo, speedMotorOne, leftStickY, leftStickX, RT, LT);
-    controlMotorForwardBackward(secondMotorOne, secondMotorTwo, speedMotorTwo, rightStickY, rightStickY, RT, LT);
-    controlMotorForwardBackward(thirdMotorOne, thirdMotorTwo, speedMotorThree, rightStickY, rightStickX, RT, LT);
-    controlMotorForwardBackward(fourthMotorOne, fourthMotorTwo, speedMotorFour, leftStickY, leftStickY, RT, LT);
+    controlMotor(motorOnePins, motorTwoPins, motorSpeedPins, leftStickY, RT, LT);
 }
 
 ros::Subscriber<sensor_msgs::Joy> sub("/joy", joyCallback);
 
 void setup() {
-    pinMode(firstMotorOne, OUTPUT);
-    pinMode(firstMotorTwo, OUTPUT);
-    pinMode(speedMotorOne, OUTPUT);
-
-    pinMode(secondMotorOne, OUTPUT);
-    pinMode(secondMotorTwo, OUTPUT);
-    pinMode(speedMotorTwo, OUTPUT);
-
-    pinMode(thirdMotorOne, OUTPUT);
-    pinMode(thirdMotorTwo, OUTPUT);
-    pinMode(speedMotorThree, OUTPUT);
-
-    pinMode(fourthMotorOne, OUTPUT);
-    pinMode(fourthMotorTwo, OUTPUT);
-    pinMode(speedMotorFour, OUTPUT);
+    for (int i = 0; i < 4; i++) {
+        pinMode(motorOnePins[i], OUTPUT);
+        pinMode(motorTwoPins[i], OUTPUT);
+        pinMode(motorSpeedPins[i], OUTPUT);
+    }
 
     Serial.begin(115200);
     WiFi.begin(ssid, password);
@@ -145,10 +98,7 @@ void setup() {
     nh.initNode();
     nh.subscribe(sub);
     
-    stopFirstMotor();
-    stopSecondMotor();
-    stopThirdMotor();
-    stopFourthMotor();
+    stopMotors(motorOnePins, motorTwoPins, motorSpeedPins);
 }
 
 void loop() {
